d4c3b5c16f849f3adea09cabc98c205b98cbd59f
Raman-Piachko <rpechko88@gmail.com>
2021-10-16 11:52:10 +0300

    initial

--- src/main/java/com/epam/rd/jsp/currencies/Currencies.java
+++ src/main/java/com/epam/rd/jsp/currencies/Currencies.java
@@ -1,26 +1,46 @@
 package com.epam.rd.jsp.currencies;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Currencies {
     private Map<String, BigDecimal> curs = new TreeMap<>();
 
     public void addCurrency(String currency, BigDecimal weight) {
-        throw new UnsupportedOperationException();
+        curs.put(currency, weight);
     }
 
     public Collection<String> getCurrencies() {
-        throw new UnsupportedOperationException();
+        return curs.keySet();
     }
 
     public Map<String, BigDecimal> getExchangeRates(String referenceCurrency) {
-        throw new UnsupportedOperationException();
+        BigDecimal refCurrencyValue = curs.get(referenceCurrency);
+        Map<String, BigDecimal> collectMap = curs.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> convertCurrency(e, refCurrencyValue)));
+
+        return new TreeMap<>(collectMap);
     }
 
     public BigDecimal convert(BigDecimal amount, String sourceCurrency, String targetCurrency) {
-        throw new UnsupportedOperationException();
+        BigDecimal source = curs.get(sourceCurrency);
+        BigDecimal target = curs.get(targetCurrency);
+        return calculateCurrency(source, target)
+                .multiply(amount)
+                .setScale(5, RoundingMode.HALF_UP);
     }
+
+    public BigDecimal calculateCurrency(BigDecimal source, BigDecimal target) {
+        return source.divide(target, 100, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal convertCurrency(Map.Entry<String, BigDecimal> entry, BigDecimal rangeValue) {
+        return rangeValue.divide(entry.getValue(), 5, RoundingMode.HALF_UP);
+    }
+
 }
--- src/main/webapp/convert.jsp
+++ src/main/webapp/convert.jsp
@@ -6,3 +6,18 @@
 <%@ page import="java.util.*, java.text.*, com.epam.rd.jsp.currencies.CurrenciesOfCurrentTestCase" %>
 
 <jsp:useBean id="currencies" class="com.epam.rd.jsp.currencies.CurrenciesOfCurrentTestCase" scope="request"/>
+<html>
+<c:set var="from" value="from" scope="request"/>
+<c:set var="to" value="to" scope="request"/>
+<c:set var="fromCurrency" value="${param.get(from)}" scope="request"/>
+<c:set var="toCurrency" value="${param.get(to)}" scope="request"/>
+<c:set var="amount" value="amount" scope="request"/>
+<c:set var="amountValue" value="${param.get(amount)}" scope="request"/>
+<c:set var="result" value="${currencies.convert(amountValue,fromCurrency,toCurrency)}"/>
+
+<head><title>Converting ${fromCurrency} to ${toCurrency}</title></head>
+<body>
+<h1>Converting ${fromCurrency} to ${toCurrency}</h1>
+Conversion amount of 1000 ${fromCurrency} to ${toCurrency} =${result}
+</body>
+</html>
